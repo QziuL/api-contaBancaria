d054baf3c0c2763acf9d39329e1393a34947b0d9
QziuL <lf.quinholi2@gmail.com>
2024-06-27 18:51:43 -0300

    fix: 'precision' e 'scale'

--- src/main/java/br/qziul/restful/model/Account.java
+++ src/main/java/br/qziul/restful/model/Account.java
@@ -16,10 +16,10 @@ public class Account {
     @Column(nullable = false)
     private String agency;
 
-    @Column(nullable = false, scale = 13, precision = 2)
+    @Column(nullable = false, precision = 13, scale = 2)
     private BigDecimal balance;
 
-    @Column(name = "additional_limit", nullable = false, scale = 13, precision = 2)
+    @Column(name = "additional_limit", nullable = false, precision = 13, scale = 2)
     private BigDecimal limit;
 
     public Long getId() {
