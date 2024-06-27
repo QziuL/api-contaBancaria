ef9da8f8c89a02678697eaa0bb9f183b8dd77dbd
QziuL <lf.quinholi2@gmail.com>
2024-06-27 13:55:48 -0300

    Initial commit

---
+++ src/main/java/br/qziul/restful/ApiRestfulSpringbootApplication.java
@@ -0,0 +1,13 @@
+package br.qziul.restful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestfulSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestfulSpringbootApplication.class, args);
+	}
+
+}
